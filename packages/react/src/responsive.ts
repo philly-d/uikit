8f5b362bf46ce8cf4646d7319592999b9af5119a
Phil Ditzler <philly-d@users.noreply.github.com>
2024-05-26 11:58:01 -0400

    Add useRootPixelSize hook

--- packages/react/src/responsive.ts
+++ packages/react/src/responsive.ts
@@ -3,3 +3,7 @@ import { useParent } from './context.js'
 export function useRootSize() {
   return useParent().root.size
 }
+
+export function useRootPixelSize() {
+  return useParent().root.pixelSize
+}
